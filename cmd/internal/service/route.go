5b4608f82d3432ec184c5139293bc3f26ef6fec1
abhishek6891 <abhisheksfs6892@gmail.com>
2025-05-15 22:45:49 +0530

    Learning to push modified file

--- cmd/internal/service/route.go
+++ cmd/internal/service/route.go
@@ -45,3 +45,34 @@ func GetSafeRoute(start, end string) (Route, error) {
 
 	return route, nil
 }
+
+func GetSafeRoute(start, end string) (Route, error) {
+	cfg := config.Load()
+	baseURL := "https://api.mapbox.com/directions/v5/mapbox/walking"
+
+	params := url.Values{}
+	params.Add("access_token", cfg.MapboxToken)
+	params.Add("geometries", "geojson")
+	params.Add("annotations", "congestion")
+	params.Add("avoid_features", "poor_lighting")
+
+	url := fmt.Sprintf("%s/%s;%s?%s", baseURL, start, end, params.Encode())
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return Route{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return Route{}, err
+	}
+
+	var route Route
+	if err := json.Unmarshal(body, &route); err != nil {
+		return Route{}, err
+	}
+
+	return route, nil
+}
